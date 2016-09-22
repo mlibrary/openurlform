0c74b24fa61c7feb096178d2a45d635fb55c322c
Earley <earleyj@m-d25q90jsfy13.local>
2016-09-22 11:19:53 -0400

    first commit

---
+++ assets/javascript/scripts.js
@@ -0,0 +1,41 @@
+/*
+
+TODO
+
+1. Show Article or Book form. Handle tabs.
+2. On submit take form fields and turn into open URL
+
+*/
+
+var tab_data = [
+  {
+    tab: 'tab1',
+    content: 'article_form'
+  },
+  {
+    tab: 'tab2',
+    content: 'book_form'
+  }
+]
+
+var handle_tabs = function(tab_data) {
+  var tab_clicked = function(tab) {
+    hide_all_content()
+
+    document.getElementById(tab.content).classList.remove('hidden')
+  }
+
+  var hide_all_content = function() {
+    _.each(tab_data, function(tab) {
+      document.getElementById(tab.content).classList.add('hidden')
+    })
+  }
+
+  // Iterate over each tab and setup event handler on tab click
+  _.each(tab_data, function(tab) {
+    var element = document.getElementById(tab.tab)
+    element.addEventListener('click', function() { tab_clicked(tab) })
+  })
+}
+
+handle_tabs(tab_data)
