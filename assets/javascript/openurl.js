31170f520ac4c4d67907205feb00a230a4f1b637
Earley <earleyj@m-d25q90jsfy13.local>
2016-10-11 13:35:02 -0400

    improvd URI encoding

--- assets/javascript/openurl.js
+++ assets/javascript/openurl.js
@@ -1,15 +1,7 @@
-/*
-
-TODO
-
-On submit take form fields and turn into open URL
-
-*/
-
 var openurl = function() {
   var settings = {
     base_url: 'https://dev.www.lib.umich.edu/testapp/mgetit/resolve',
-    referrer_id: 'https://dev.www.lib.umich.edu'
+    sid: 'info:sid:lib.umich.edu'
   }
 
   var create = function(type, query_string_hash) {
@@ -20,15 +12,16 @@ var openurl = function() {
     return settings.base_url
                 + '?'
                 + get_format_query(type)
+                + '&sid=' + encodeURIComponent(settings.sid)
                 + create_query_strings().generate(query_string_hash)
   }
 
   var get_format_query = function(type) {
     switch (type) {
       case 'journal':
-        return 'rft_val_fmt=info:ofi/fmt:kev:mtx:journal&Genre=Article'
+        return encodeURIComponent('rft_val_fmt') + '=' + encodeURIComponent('info:ofi/fmt:kev:mtx:journal') + '&rft.genre=Article'
       case 'book':
-        return 'rft_val_fmt=info:ofi/fmt:kev:mtx:book&Genre=Book'
+        return encodeURIComponent('rft_val_fmt') + '=' + encodeURIComponent('info:ofi/fmt:kev:mtx:book') + '&rft.genre=Book'
     }
   }
 
@@ -47,44 +40,26 @@ var create_query_strings = function() {
       throw "query string missing value"
     }
 
-    // DOI's are special snowflakes
+    // DOIs and PMIDs are special snowflakes
     if (value.key == 'doi') {
       // Example: &rft_id=info:doi=10.1111/1468-232X.00264
-      return '&' + escape('rft_id') + '=info:doi=' + escape(value.value)
+      return '&' + encodeURIComponent('rft_id') + '=' + encodeURIComponent('info:doi=' + value.value)
     }
 
-    // PMID is also a special snowflakes
     if (value.key == 'pmid') {
       // Example: &rft_id=info:pmid=27405801
-      return '&' + escape('rft_id') + '=info:pmid/' + escape(value.value)
+      return '&' + encodeURIComponent('rft_id') + '=' + encodeURIComponent('info:pmid/' + value.value)
     }
 
-    return '&' + escape(value.key) + '=' + escape(value.value)
+    return '&' + encodeURIComponent(value.key) + '=' + encodeURIComponent(value.value)
   }
 
-  /*
-    Example
-
-    query_string_hash = [
-      {
-        key: "title",
-        value: "Hello World"
-      }
-    ]
-  */
   var generate = function(query_string_hash) {
 
     return _.reduce(query_string_hash, function(base, value) {
       return base = base + create_query_string(value)
     }, '')
   }
-
-  var escape = function(string) {
-    var temp_element = document.createElement('div');
-    temp_element.appendChild(document.createTextNode(string));
-
-    return temp_element.innerHTML;
-  };
 
   return {
     generate: generate
